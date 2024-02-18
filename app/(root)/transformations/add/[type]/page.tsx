e34ef1bef944ad7eee5fa5f6242942b842944474
felixNyalenda <felixasejunior@gmail.com>
2024-02-18 09:05:37 +0300

    tranformation page inputs init

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,15 +1,30 @@
 import { Header, TransformationForm } from "@/components/shared";
 import { transformationTypes } from "@/constants";
-import { SearchParamProps } from "@/types";
+import { getUserById } from "@/lib/actions/user.action";
+import { SearchParamProps, TransformationTypeKey } from "@/types";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const AddTransformationType = ({ params: { type } }: SearchParamProps) => {
-  const { title, subtitle } = transformationTypes[type];
+const AddTransformationType = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const { userId } = auth();
+  const transformation = transformationTypes[type];
+  const { title, subtitle } = transformation;
+  if (!userId) redirect("/sign-in");
+  const user = await getUserById(userId);
 
   return (
     <>
       <Header title={title} subtitle={subtitle} />
-      <TransformationForm />
+      <TransformationForm
+        userId={user._id}
+        // data={}
+        action="Add"
+        type={transformation.type as TransformationTypeKey}
+        creditBalance={user.creditBalance}
+      />
     </>
   );
 };
--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -1,7 +1,70 @@
-import React from "react";
+"use client";
 
-const TransformationForm = () => {
-  return <div>TransformationForm</div>;
+import React from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { Button } from "../ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { defaultValues } from "@/constants";
+import { TransformationFormProps } from "@/types";
+import { CustomField } from "./CustomField";
+
+export const formSchema = z.object({
+  title: z.string(),
+  aspectRatio: z.string().optional(),
+  color: z.string().optional(),
+  prompt: z.string().optional(),
+  publicId: z.string(),
+});
+
+const TransformationForm = ({
+  action,
+  data = null,
+}: TransformationFormProps) => {
+  const initialValues =
+    data && action === "Update"
+      ? {
+          title: data?.title,
+          aspectRatio: data?.aspectRatio,
+          color: data?.color,
+          prompt: data?.prompt,
+          publicId: data?.publicId,
+        }
+      : defaultValues;
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialValues,
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <CustomField
+          control={form.control}
+          render={({ field }) => <Input {...field} className="input-field" />}
+          name="title"
+          formLabel="Image Title"
+          className="w-full"
+        />
+      </form>
+    </Form>
+  );
 };
 
 export default TransformationForm;
