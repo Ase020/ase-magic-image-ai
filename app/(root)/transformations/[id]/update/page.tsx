a138d73ad6973df31190cb9f9d0f12f5cc808838
felixNyalenda <felixasejunior@gmail.com>
2024-02-22 17:56:25 +0300

    update image functionality

--- app/(root)/transformations/[id]/update/page.tsx
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const UpdateTransformations = () => {
-  return <div>UpdateTransformations</div>;
+import { transformationTypes } from "@/constants";
+import { getUserById } from "@/lib/actions/user.action";
+import { getImageById } from "@/lib/actions/image.action";
+import { Header, TransformationForm } from "@/components/shared";
+
+const Page = async ({ params: { id } }: SearchParamProps) => {
+  const { userId } = auth();
+
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
+  const image = await getImageById(id);
+
+  const transformation =
+    transformationTypes[image.transformationType as TransformationTypeKey];
+
+  return (
+    <>
+      <Header title={transformation.title} subtitle={transformation.subtitle} />
+
+      <section className="mt-10">
+        <TransformationForm
+          action="Update"
+          userId={user._id}
+          type={image.transformationType as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+          config={image.config}
+          data={image}
+        />
+      </section>
+    </>
+  );
 };
 
-export default UpdateTransformations;
+export default Page;
